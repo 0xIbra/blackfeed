f2a374b3aa1caf94939d3365b67e082b074c36c1
Ibragim <ibragim.ai95@gmail.com>
2020-01-03 15:41:49 +0100

    Minor packages patch

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='blackfeed',
-    version='0.0.8',
+    version='0.0.9',
     author='Ibragim Abubakarov',
     author_email='ibragim.ai95@gmail.com',
-    description='A micro python library that allow the download of thousands of files concurrently',
+    description='A python package that allows the download of thousands of files concurrently',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ibragim64/blackfeed',
-    packages=['blackfeed'],
+    packages=['blackfeed', 'blackfeed.adapter', 'blackfeed.helper'],
     install_requires=['requests', 'boto3'],
     classifiers=[
         "Programming Language :: Python :: 3"
